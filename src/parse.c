41c5b85174060a4b3897ddca70842b480fd2c652
Quentin Ruiz <quruiz@student.le-101.fr>
2019-01-04 18:10:29 +0100

    🐌

--- src/parse.c
+++ src/parse.c
@@ -1,44 +1,15 @@
 /* ************************************************************************** */
 /*                                                          LE - /            */
 /*                                                              /             */
-/*   asm.c                                            .::    .:/ .      .::   */
+/*   parse.c                                          .::    .:/ .      .::   */
 /*                                                 +:+:+   +:    +:  +:+:+    */
 /*   By: quruiz <quruiz@student.le-101.fr>          +:+   +:    +:    +:+     */
 /*                                                 #+#   #+    #+    #+#      */
-/*   Created: 2018/12/16 16:58:11 by quruiz       #+#   ##    ##    #+#       */
-/*   Updated: 2019/01/04 16:12:27 by quruiz      ###    #+. /#+    ###.fr     */
+/*   Created: 2019/01/04 16:30:54 by quruiz       #+#   ##    ##    #+#       */
+/*   Updated: 2019/01/04 18:00:32 by quruiz      ###    #+. /#+    ###.fr     */
 /*                                                         /                  */
 /*                                                        /                   */
 /* ************************************************************************** */
 
 #include "../include/asm.h"
 
-int		err_code(int code, char *msg)
-{
-	if (code == 0)
-		printf("Error malloc\n");
-	if (code == 1)
-		printf("Usage: %s <file>.s\n", msg);
-	if (code == 2)
-		printf("Can't read source file %s\n", msg);
-	if (code == 3)
-		printf("Empty source file %s\n", msg);
-	return (0);
-}
-
-extern t_op		g_op_tab[17];
-
-int		main(int argc, char *argv[])
-{
-	t_asm	*env;
-	char	*file;
-
-	if (argc != 2)
-		return (err_code(1, argv[0]));
-	if (!read_file(&env, argv))
-		return (1);
-	/*
-	**	Parsing
-	*/
-	ft_putsplit(env->file);
-}

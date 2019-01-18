7b3fef17ca6aec4728d691148d0815327258a996
Quentin Ruiz <quruiz@student.le-101.fr>
2019-01-18 18:50:32 +0100

    🐌

--- src/destructor.c
+++ src/destructor.c
@@ -6,24 +6,19 @@
 /*   By: quruiz <quruiz@student.le-101.fr>          +:+   +:    +:    +:+     */
 /*                                                 #+#   #+    #+    #+#      */
 /*   Created: 2019/01/16 19:25:08 by quruiz       #+#   ##    ##    #+#       */
-/*   Updated: 2019/01/16 19:40:20 by quruiz      ###    #+. /#+    ###.fr     */
+/*   Updated: 2019/01/17 15:11:56 by quruiz      ###    #+. /#+    ###.fr     */
 /*                                                         /                  */
 /*                                                        /                   */
 /* ************************************************************************** */
 
 #include "../include/asm.h"
 
-void    free_env(t_asm *env)
-{
-    if (env->header)
-        free(env->header);
-    free(env->name);
-    free(env);
-}
-
 void    free_all(t_asm *env)
 {
-    // if (env->code)
-        // free_code(env);
-    free_env(env);
+	// if (env->code)
+	// 	free_code(env);
+	if (env->header)
+		free(env->header);
+	free(env->name);
+	free(env);
 }
--- src/header.c
+++ src/header.c
@@ -6,7 +6,7 @@
 /*   By: quruiz <quruiz@student.le-101.fr>          +:+   +:    +:    +:+     */
 /*                                                 #+#   #+    #+    #+#      */
 /*   Created: 2019/01/11 16:13:28 by quruiz       #+#   ##    ##    #+#       */
-/*   Updated: 2019/01/16 18:16:58 by quruiz      ###    #+. /#+    ###.fr     */
+/*   Updated: 2019/01/18 18:48:45 by quruiz      ###    #+. /#+    ###.fr     */
 /*                                                         /                  */
 /*                                                        /                   */
 /* ************************************************************************** */
@@ -42,14 +42,76 @@
 // 	return (1);
 // }
 
+char	*read_more(t_asm *env, char *line, int len)
+{
+	char	*tmp;
+	int		size[3];
+
+	size[0] = ft_strlen(line);
+	size[2] = ft_strlen(ft_strchr(line, '"'));
+	while (get_next_line(env->input_fd, &tmp))
+	{
+		env->error_line++;
+		size[1] = ft_strlen(tmp);
+		line = ft_conncat(line, tmp, size[0], size[1]);
+		if (ft_strchr(tmp, '"'))
+			return (line);
+		size[0] += size[1];
+		size[2] += size[1];
+		if (size[2] > len)
+			break ;
+	}
+	return (NULL);
+}
+
+char	*read_header(t_asm *env, char *line, char *cmd, int len)
+{
+ 	char	*start;
+	char	*end;
+	char	*str;
+
+	if (!(start = ft_strstr(line, cmd)))
+		return (NULL);
+	if (!(start = ft_strchr(start, '"')))
+		return (NULL);
+    if (!(end = ft_strchr(start + 1, '"')))
+	{
+		if (!(line = read_more(env, line, len)))
+			return (NULL);
+		return (read_header(env, line, cmd, len));
+	}
+	else if (end[1] != '\0')
+		return (NULL);
+	str = ft_strsub(start, 1, end - (start + 1));
+	if (ft_strlen(str) > len)
+	{
+		free(str);
+		return (NULL);
+	}
+	return (str);
+}
+
 int		get_header(t_asm *env, char *line)
 {
+	char	*res;
+
 	if (!(env->header = ft_memalloc(sizeof(t_header))))
 		return (err_code(0, env));
 	env->header->magic = COREWAR_EXEC_MAGIC;
-    printf("bite: %s -> %zu\n", line, ft_strlen(line));
     if (*line != '.')
         return (err_code(4, env));
-    /* lis tout le reste du header */
+	if (!(res = read_header(env, line, NAME_CMD_STRING, PROG_NAME_LENGTH)))
+		return (err_code(4, env));
+	ft_strcpy(env->header->prog_name, res);
+	free(res);
+
+	get_next_line(env->input_fd, &line);
+	env->error_line++;						/* need better read */
+
+	if (!(res = read_header(env, line, COMMENT_CMD_STRING, COMMENT_LENGTH)))
+		return (err_code(4, env));
+	ft_strcpy(env->header->comment, res);
+	free(res);
+    dprintf(1, "NAME: %s\nCOMMENT: %s\n", env->header->prog_name, env->header->comment);
 	return (1);
 }
